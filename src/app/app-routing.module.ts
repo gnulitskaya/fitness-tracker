abcce3d8004e903476419fe84f61fd6b709aba5d
Flashy <khoruzhenko.work@gmail.com>
2022-01-16 20:12:18 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { TrainingComponent } from './training/training.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignComponent } from './auth/sign/sign.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'training', component: TrainingComponent},
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
